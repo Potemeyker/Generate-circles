cd6a3f489bd9c9aae053186bd8669d024a5bf7dc
ASUS-Notebook <nkazunin.n@gmail.com>
2024-02-08 10:11:56 +0300

    Change main.py

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, QPushButton, QVBoxLayout
-from PyQt5.QtGui import QColor
-from PyQt5.uic import loadUi
 import random
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QGraphicsScene, QGraphicsView,
+                             QGraphicsEllipseItem)
+from PyQt5.QtGui import QColor
 
 
-class CircleWidget(QWidget):
+class CircleGenerator(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.setWindowTitle('Random Circles Generator')
+        self.setGeometry(100, 100, 400, 400)
 
-    def initUI(self):
-        loadUi('UI.ui', self)
-
+        self.button = QPushButton('Generate Circle', self)
         self.button.clicked.connect(self.generate_circle)
 
         self.scene = QGraphicsScene(self)
-        self.view.setScene(self.scene)
+        self.view = QGraphicsView(self.scene)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.button)
+        layout.addWidget(self.view)
 
     def generate_circle(self):
         diameter = random.randint(10, 100)
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
         circle = QGraphicsEllipseItem(0, 0, diameter, diameter)
         circle.setPos(random.randint(0, 400 - diameter), random.randint(0, 400 - diameter))
-        circle.setBrush(QColor('yellow'))
+        circle.setBrush(color)
+
         self.scene.addItem(circle)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = CircleWidget()
+    window = CircleGenerator()
     window.show()
     sys.exit(app.exec_())
